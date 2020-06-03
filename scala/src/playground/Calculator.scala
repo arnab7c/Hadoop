0815aaea618a89e37fa478bd478b932b95576859
Arnab Chatterjee <arnab.linux@gmail.com>
2020-06-03 11:10:22 +0530

    Scala basics 3

--- scala/src/playground/Calculator.scala
+++ scala/src/playground/Calculator.scala
@@ -1,5 +1,9 @@
 /**
-  *
+  * Anonymous Function
+  * Higher Order Function, partial application of function
+  * for - yield
+  * closure
+  * partial function
   */
 package playground
 
@@ -8,6 +12,7 @@ class Calculator {
 	val substractor = (_:Int) - (_:Int) 
 	// placeholder, replacement for (a,b) => a - b,here types are mandatory cause scala doesn't have something to infer from
 	
+	def add3val : (Int,Int,Int) => Int = (a,b,c) => a+b+c
 	
 	println(adder(4,5))
 	println(substractor(7,4))
@@ -22,9 +27,16 @@ class Calculator {
 	
 	val tripple = list.map(_ * 3) // placeholder, no need to declare type
 	
+	val adval1 = add3val(_,_,_) // partial application of function
+	val adval2 = add3val(5,_:Int,8) // second parameter supplied during call
+	val adval3 = add3val // all parameters supplied during call
+	
 	println("Square -> "+square)  
 	println("Double -> "+double)  
 	println("Tripple -> "+tripple)
+	println("Adval 1-> "+adval1(4,5,6))
+	println("Adval 2-> "+adval2(10))
+	println("Adval 3-> "+adval3(10,20,30))
 }
 
 object Calculator { // companion object
@@ -43,6 +55,31 @@ object Calculator { // companion object
 				}
 		}.toList
 	
-
 		
+	// Closure - A closure is so-called because it encloses around some other state than that passed into the function as parameters	
+	// It's better alternative for vars in scala
+		
+		val total = 70
+		
+		val parcentage = (x:Int) => (x * 100)/total // this is closure, total is passed from other state
+		
+		println("Parcentage -> "+parcentage(55))
+	
+	// Partial Function -> Any block of code with case inside of {}s is a Partial Function
+	// A PartialFunction[T, R] extends Function1[T, R] (which is idiomatically written T => R)
+	// MatchError thrown if there is no case to handle the input 
+		
+		def GradeCalculation (x:Int): String = 
+			x match {
+				case x if ( x >= 90 ) => s"O"
+				case x if ( x >= 80 && x < 90 ) => s"E"
+				case x if ( x >= 70 && x < 80 ) => s"A"
+				case x if ( x >= 60 && x < 70 ) => s"B"
+				case x if ( x >= 50 && x < 60 ) => s"C"
+				case x if ( x >= 40 && x < 50 ) => s"D"
+				case _ => s"F"
+			}
+			
+		
+		println("Grade -> "+GradeCalculation(parcentage(10)))
 }
